43ba423b2bfc0aaff2b8ecf942730f08d9006fbc
marine <dedola.marine@hotmail.com>
2021-03-08 00:05:14 +0100

    Connexion Spotify

--- mindle-front/src/App.js
+++ mindle-front/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MainActivity from './components/MainActivity'
+import ConnexionActivity from './components/ConnexionActivity'
 
 function App() {
+
+  const [connexionStatus, setConnexionStatus] = useState(false)
+
+  const handleConnexionStatus = () => {
+    setConnexionStatus(!connexionStatus)
+  }
+
   return (
     <div>
-      <MainActivity />
+      {
+        connexionStatus ? <MainActivity /> : <ConnexionActivity handleConnexionStatus={handleConnexionStatus} />
+      }
     </div>
   );
 }
